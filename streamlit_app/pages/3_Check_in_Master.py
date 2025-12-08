dfb18a3f7c83f89bfef712330cd3b84461b15494
vedprathap28 <143394680+vedprathap28@users.noreply.github.com>
2025-12-08 18:22:28 +0530

    Update 3_Check_in_Master.py

--- streamlit_app/pages/3_Check_in_Master.py
+++ streamlit_app/pages/3_Check_in_Master.py
@@ -4,7 +4,8 @@ import requests
 import tempfile
 import os
 
-API_BASE = "http://127.0.0.1:8000"
+API_BASE = st.secrets.get("API_BASE_URL", os.getenv("API_BASE_URL", "http://127.0.0.1:8000"))
+
 
 st.set_page_config(page_title="Check in Master", layout="wide")
 st.title("✅ Check Questions in Master Sheet (Unit-wise)")
